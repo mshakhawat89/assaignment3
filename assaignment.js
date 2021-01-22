0469b673ab78def1afbd23e4d89458390d3b415c
md shakhawat hossain <hossainmdshakhawat19@gmail.com>
2021-01-22 12:42:46 +0600

    my javascript assaignment

---
+++ assaignment.js
@@ -0,0 +1,95 @@
+
+/**       Bellow is My 1st task       */
+
+
+function kilometerToMeter(kilommeter){
+
+ var meter = kilommeter*1000;
+
+return meter;
+}
+
+var result = kilometerToMeter(15);
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+/**                My 2nd task             */
+ 
+
+
+
+
+function budgetCalculator(n){
+
+var watchCostings   = 50 * n;
+var mobileCostings = 100 * n;
+var laptopCostings = 500 * n;
+
+var totalCostings = watchCostings + mobileCostings + laptopCostings;
+
+return totalCostings;
+}
+
+var budget = budgetCalculator(8);
+console.log(budget);
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**          My 3rd task is given bellow...            */
+
+
+function hotelCost(night){
+cost = 0;
+
+ if( night<=10){
+ cost = night * 100;
+
+ }
+else if(night<= 20){
+ var firstCost = 10*100;
+ var remaining = night -10;
+ var secondCost = remaining* 80;
+ cost = firstCost + secondCost;
+
+}
+else{
+var firstCost = 10*100;
+var secondCost =10*80;
+var remaining = night - 20;
+var thirdCost = remaining*50;
+cost = firstCost + secondCost + thirdCost;
+}
+
+return cost;
+}
+
+var total = hotelCost(35);
+console.log(total);
+
+
+
+
+
+
+
+
+/**       This is my last task for the day      */
